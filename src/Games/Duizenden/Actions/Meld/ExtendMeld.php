9f1f3770c1bfac55033b691a3f1c31dc8499f793
Ron Gähler <t_ronx@hotmail.com>
2020-03-10 21:55:22 +0100

    Fixed issue where an invalid meld could be made when existing an existing meld due to joker reordering.

--- src/Games/Duizenden/Actions/Meld/ExtendMeld.php
+++ src/Games/Duizenden/Actions/Meld/ExtendMeld.php
@@ -6,12 +6,12 @@ namespace  App\Games\Duizenden\Actions\Meld;
 
 use App\CardPool\Exception\CardNotFoundException;
 use App\Common\Meld\Exception\InvalidMeldException;
+use App\Common\Meld\MeldType;
 use App\Deck\Card\CardInterface;
 use App\Games\Duizenden\Actions\StateChangeAction;
 use App\Games\Duizenden\Exception\HandException;
 use App\Games\Duizenden\Game;
 use App\Games\Duizenden\Meld\Exception\MeldException;
-use App\Games\Duizenden\Meld\OrderHelper;
 use App\Games\Duizenden\Meld\TypeHelper;
 use App\Games\Duizenden\State;
 use App\Games\Duizenden\Workflow\TransitionType;
@@ -47,12 +47,8 @@ class ExtendMeld extends StateChangeAction
 	private function extendMeld(State $state, int $meld_id, CardInterface $card): void
 	{
 		$meld = $state->getPlayers()->getCurrentPlayer()->getMelds()->get($meld_id);
-
 		$cards = $meld->getCards()->getCards();
-		$cards[] = $card;
-
-		OrderHelper::orderCards($cards);
-		$meld_type = TypeHelper::detectMeldType($cards);
+        $meld_type = $this->createNewMeld($cards, $card);
 
 		if (!$meld_type)
 		{
@@ -64,6 +60,51 @@ class ExtendMeld extends StateChangeAction
 
 		$meld->getCards()->setCards($cards);
 	}
+
+    /**
+     * @param CardInterface[] $cards
+     * @param CardInterface $new_card
+     */
+	private function createNewMeld(array &$cards, CardInterface $new_card): ?MeldType
+    {
+        return $this->tryNewCardAtStart($cards, $new_card) ?: $this->tryNewCardAtEnd($cards, $new_card);
+    }
+
+    /**
+     * @param CardInterface[] $cards
+     * @param CardInterface $new_card
+     */
+    private function tryNewCardAtStart(array &$cards, CardInterface $new_card): ?MeldType
+    {
+        $test_cards = $cards;
+        array_unshift($test_cards, $new_card);
+        $meld_type = TypeHelper::detectMeldType($test_cards);
+
+        if ($meld_type)
+        {
+            $cards = $test_cards;
+        }
+
+        return $meld_type;
+    }
+
+    /**
+     * @param CardInterface[] $cards
+     * @param CardInterface $new_card
+     */
+    private function tryNewCardAtEnd(array &$cards, CardInterface $new_card): ?MeldType
+    {
+        $test_cards = $cards;
+        array_push($test_cards, $new_card);
+        $meld_type = TypeHelper::detectMeldType($test_cards);
+
+        if ($meld_type)
+        {
+            $cards = $test_cards;
+        }
+
+        return $meld_type;
+    }
 
 	/**
 	 * @param CardInterface[] $cards
